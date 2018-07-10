2e5e00b98bdd03b4f722f2b2b54da38682e5c178
Sam Rose <samuel.rose@gmail.com>
2018-07-10 09:54:53 -0400

    testing the outcome of pkgs.releaseTools.* functions in this throwaway build

---
+++ holo/holo.nix
@@ -0,0 +1,24 @@
+let
+  pkgs = import <nixpkgs> {};
+  jobs = rec {
+
+    tarball = 
+      pkgs.releaseTools.sourceTarball {
+        name = "test-tarball";
+        srv = <hello>;
+        buildInputs = (with pkgs; [ gettext texLive texinfo)]);
+      };
+    
+    build = 
+      { system ? builtins.current.System}:
+
+      let pkgs = import <nixpkgs> {inherit system; }; in
+      pkgs.releaseTools.nixBuild {
+        name = "hello";
+        src = jobs.tarball;
+        configureFlahs = [ "--disable-silent-rules" ];
+      };
+  };
+in
+  jobs
+
